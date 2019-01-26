e579032570f30b359a0e54738faf017332afbf83
norest <orestisef@gmail.com>
2019-01-26 11:36:35 +0200

    working with JSON

--- index.js
+++ index.js
@@ -19,10 +19,30 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response, body){
-console.log(response.statusCode);
+
+  var crypto = req.body.crypto;
+  var fiat = req.body.fiat;
+
+  var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+
+  var finalURL = baseURL + crypto + fiat;
+
+  request(finalURL, function(error, response, body) {
+
+    var data = JSON.parse(body);
+    var price = data.last;
+
+    var currentDate = data.display_timestamp;
+
+
+    res.write("<p>The current date is " + currentDate + "</p>");
+    res.write("<h1> The price of " + crypto + " is " + price + " " + fiat + "</h1>");
+
+    res.send();
   });
 });
+
+
 
 
 app.listen(3000, function() {
